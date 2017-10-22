8b1217908ce5e142f465e4a2200e0615c3626183
jackson meyer <jackson.meyer7@gmail.com>
2017-10-22 00:39:43 -0700

    Added the first screen of the ARKit, shows feature points and xyzzy

--- WorldTRacking/ViewController.swift
+++ WorldTRacking/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        
+        self.sceneView.session.run(configuration)
     }
 
     override func didReceiveMemoryWarning() {
