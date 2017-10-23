4672d6819b524067c390426b733fa29b748a331f
jackson meyer <jackson.meyer7@gmail.com>
2017-10-23 00:20:52 -0700

    Added two buttons to the SCNView and hooked them up to add a floating box positioned to origin, and then a reset tracking button

--- WorldTRacking/ViewController.swift
+++ WorldTRacking/ViewController.swift
@@ -21,12 +21,24 @@ class ViewController: UIViewController {
         
         self.sceneView.session.run(configuration)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addBox(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 0.03)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.black
+        node.position = SCNVector3(0.6,0.6,0.0)
+        self.sceneView.scene.rootNode.addChildNode(node)
     }
-
-
+    @IBAction func reset(_ sender: Any) {
+        self.resetSession()
+    }
+    
+    
+    func resetSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes{(node, _) in
+            node.removeFromParentNode()
+        }
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
 }
 
