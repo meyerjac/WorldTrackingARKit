c762a8f11f2558f6e9412f3bd76255178c1d737c
jackson meyer <jackson.meyer7@gmail.com>
2017-10-24 00:41:03 -0700

    Added child nodes to parent nodes and built custom shapes, right now it is currently a little house floating above the world origin.

--- WorldTRacking/ViewController.swift
+++ WorldTRacking/ViewController.swift
@@ -23,20 +23,46 @@ class ViewController: UIViewController {
     }
     
     @IBAction func addBox(_ sender: Any) {
+        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.00))
+        let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0.03)
+//        let path = UIBezierPath()
+//        node.geometry = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0.03)
+//        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+//        node.geometry = SCNCone(topRadius: 0.0, bottomRadius: 0.3, height: 0.5)
+//        node.geometry = SCNSphere(radius: 0.2)
+//        node.geometry = SCNPlane(width: 1.0, height: 1.0)
+//        path.move(to: CGPoint(x: 0.0, y: 0.0))
+//        path.addLine(to: CGPoint(x:0.0, y: 0.1))
+//        path.addLine(to: CGPoint(x:0.2, y: 0.2))
+//        path.addLine(to: CGPoint(x: 0.4, y: 0.1))
+//        path.addLine(to: CGPoint(x: 0.4, y: 0.0))
+//        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+//        node.geometry = shape
+        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
         node.geometry?.firstMaterial?.specular.contents = UIColor.white
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        node.position = SCNVector3(x,y,z)
+        
+        boxNode.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        
+        doorNode.geometry?.firstMaterial?.specular.contents = UIColor.white
+        doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+       
+//        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+//        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+//        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        node.position = SCNVector3(0.2,0.3,-0.2)
+        boxNode.position = SCNVector3(0, -0.05, 0)
+        doorNode.position = SCNVector3(0, -0.07, 0.052)
         self.sceneView.scene.rootNode.addChildNode(node)
+        node.addChildNode(boxNode)
+        node.addChildNode(doorNode)
+//        self.sceneView.scene.rootNode.addChildNode(cylinderNode)
     }
     @IBAction func reset(_ sender: Any) {
         self.resetSession()
     }
-    
     
     func resetSession() {
         self.sceneView.session.pause()
